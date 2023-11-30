bd804959285db24dbbde0e4b914955828999b0f7
shirinvatani <shirinvatani97@gmail.com>
2023-12-01 02:31:08 +0330

    Home View

--- app/src/main/java/com/example/newsappwithcompose/presentation/Dimans.kt
+++ app/src/main/java/com/example/newsappwithcompose/presentation/Dimans.kt
@@ -8,6 +8,8 @@ object Dimans {
     val MediumPadding2= 30.dp;
     val ArticleCardSize= 96.dp;
     val ExtraSmallPadding= 3.dp;
+    val ExtraSmallPadding2= 6.dp;
+    val SmallIconSize= 11.dp;
 
     val IndicatorSize= 40.dp;
 }
--- app/src/main/java/com/example/newsappwithcompose/presentation/common/Article.kt
+++ app/src/main/java/com/example/newsappwithcompose/presentation/common/Article.kt
@@ -6,25 +6,34 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.example.newsappwithcompose.R
 import com.example.newsappwithcompose.domain.model.Article
 import com.example.newsappwithcompose.domain.model.Source
 import com.example.newsappwithcompose.presentation.Dimans.ArticleCardSize
 import com.example.newsappwithcompose.presentation.Dimans.ExtraSmallPadding
+import com.example.newsappwithcompose.presentation.Dimans.ExtraSmallPadding2
+import com.example.newsappwithcompose.presentation.Dimans.SmallIconSize
 import com.example.newsappwithcompose.ui.theme.NewsAppWithComposeTheme
 
 
@@ -56,6 +65,27 @@ fun ArticalCard(
                 color = Color.Black
             , maxLines = 2,
                 overflow = TextOverflow.Ellipsis)
+            Row (verticalAlignment = Alignment.CenterVertically){
+                Text(text = article.source.name,
+                    style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Bold),
+                    color = Color.Blue
+                    , maxLines = 2,
+                    overflow = TextOverflow.Ellipsis)
+
+                Spacer(modifier = Modifier.width(ExtraSmallPadding2))
+           Icon(painter = painterResource(id = R.drawable.ic_time),
+               contentDescription = null,
+               modifier = Modifier.size(SmallIconSize),
+               tint = Color.Gray)
+
+                Spacer(modifier = Modifier.width(ExtraSmallPadding2))
+
+                Text(text = article.publishedAt,
+                    style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Bold),
+                    color = Color.Blue
+                    , maxLines = 2,
+                    overflow = TextOverflow.Ellipsis)
+            }
         }
     }
 }
