816b17f9942bceec8ecd0679fab7361f2655d33f
shirinvatani <shirinvatani97@gmail.com>
2023-12-03 23:56:10 +0330

    Detail View Model

--- app/src/main/java/com/example/newsappwithcompose/domain/usecase/news/DeleteArticle.kt
+++ app/src/main/java/com/example/newsappwithcompose/domain/usecase/news/DeleteArticle.kt
@@ -15,4 +15,3 @@ class DeleteArticle (
             newsDao.delete(article)
         }
     }
-}
---
+++ app/src/main/java/com/example/newsappwithcompose/presentation/details/DetailsViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.newsappwithcompose.presentation.details
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.newsappwithcompose.domain.usecase.news.NewCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class DetailsViewModel @Inject constructor(
+    private val newsCases: NewCases
+): ViewModel() {
+
+
+
+    var sideEffect by mutableStateOf<String?>( null)
+
+
+}
